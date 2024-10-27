94600c280e7c05097cb8ee251764994539f91782
Samuele Barzaghi <sambarza@gmail.com>
2024-10-27 19:18:10 +0100

    Only one session and one commit

--- insert_records_async.py
+++ insert_records_async.py
@@ -11,25 +11,28 @@ SessionLocal = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSes
 from models import User
 
 # Insert function to add data asynchronously
-async def insert_user(name: str, email: str):
-    async with SessionLocal() as session:
-        async with session.begin():
-            new_user = User(name=name, email=email)
-            session.add(new_user)
-            await session.commit()
-            print(f"Inserted user: {name}, {email}")
+async def insert_user(name: str, email: str, session):
+    
+    new_user = User(name=name, email=email)
+    session.add(new_user)
+    print(f"Inserted user: {name}, {email}")
 
 async def main():
 
     inserts = []
 
-    for i in range(100):
-        inserts.append(insert_user("Sam", f"sam.{i}@example.com"))
+    async with SessionLocal() as session:
 
-    try:
-        await asyncio.gather(*inserts)
-    finally:
-        await engine.dispose()
+        async with session.begin():
+             
+            for i in range(10000):
+                inserts.append(insert_user("Sam", f"sam.{i}@example.com", session=session))
+
+        try:
+            await asyncio.gather(*inserts)
+            await session.commit()
+        finally:
+            await engine.dispose()
 
 # Run the main function with asyncio
 if __name__ == "__main__":
